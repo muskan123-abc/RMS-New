0602819115b29124de00e3330b01f1f62d02b199
Muskan Saini <muskansaini@Muskans-MacBook-Air.local>
2024-02-12 10:01:17 +0530

    do the setup of the router and tailwind css

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
-export default Hero
+export default Hero;
