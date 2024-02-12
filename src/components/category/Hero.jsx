3e95e3eab93a9b40e63a465789b5ec1ec7dc084b
Rakesh Jaat <rakesh10448olla@gail.com>
2024-02-12 14:33:34 +0530

    hero section added

--- src/components/category/Hero.jsx
+++ src/components/category/Hero.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import category_hero_reel_img from "../../assets/images/png/category_hero_reel_img.png";
+import category_hero_img from "../../assets/images/webp/category_hero_img.webp";
+import CustomInput from "../common/CustomInput";
+import { CommonXmildIcon } from "../common/Icon";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <>
+      <section className="min-h-[calc(100vh-76px)] min-[1440px]:min-h-screen min-[1440px]:pl-4 bg-homepage-hero-bg bg-cover bg-no-repeat bg-center relative z-10 flex min-[1440px]:pt-[76px] items-center">
+        <div className="bottom_blur_hero w-full h-[50px] absolute bottom-0 left-0 z-50"></div>
+        <span className="absolute left-0 top-0 min-[1440px]:top-[74px] ps-5 md:pt-5 pt-10 hidden lg:block movieIcon_animation">
+          <CommonXmildIcon />
+        </span>
+        <img
+          className="w-[240px] h-[381px] object-cover absolute end-0 bottom-[110px] 2xl:bottom-[5%] xl:bottom-[20%] -z-0  hidden xl:block"
+          src={category_hero_reel_img}
+          alt="popular_reel_position_img"
+        />
+        <div className="container max-w-full xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto flex-grow flex justify-center items-center h-full">
+          <div className="flex flex-col-reverse md:flex-row justify-between items-center ">
+            <div className="w-full sm:w-[75%] md:w-1/2 lg:w-[39%] pt-10 md:pt-0 pb-12 md:pb-0">
+              <h2 className="ff_poppins font-bold text-[35px] lg:text-[40px] xl:text-[55px] text-white leading-[142%]">
+                TV <span className="common-text-gradient">Shows</span>
+              </h2>
+              <p className=" ff_poppins text-[16px] font-normal opacity-70 text-white pt-3 leading-[160%] mb-10">
+                Sit volutpat purus semper metus laoreet amet justo suscipit
+                posuere vestibulum eget erat aliquet quam donec donec
+                suspendisse.
+              </p>
+              <div className="hidden md:flex">
+                <CustomInput placeholder="Search" width="w-full md:w-[80%]" />
+              </div>
+            </div>
+            <div className=" w-full sm:w-[70%] md:w-[45%] ">
+              <div className="md:hidden my-10">
+                <CustomInput placeholder="Search" width="w-full md:w-[80%]" />
+              </div>
+              <div className="border-2 border-solid border-[#FFFFFF4D] rounded-lg z-50 relative overflow-hidden">
+                <img
+                  height={523}
+                  width={523}
+                  className="w-full h-full hover:scale-110 duration-300 z-20"
+                  src={category_hero_img}
+                  alt="hero-img"
+                  loading="lazy"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
