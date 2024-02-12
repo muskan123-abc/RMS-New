55efc5a3cabb0a02ecdb83f1c1e33de160ff8bd2
mk0029 <mkhicher404@gmail.com>
2024-02-12 11:41:00 +0530

    add custom button

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import CustomButton from "./fields/button/CustomButton";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      Header
+      <div className="flex gap-3 flex-wrap">
+        {" "}
+        <CustomButton
+          title="Lorem"
+          isGradient={true}
+          varient="button"
+          url="//##/"
+          isVisible={true}
+          customStyles=""
+        />
+        <CustomButton
+          title="Ipsum doller sit ammit"
+          isGradient={false}
+          varient="link"
+          url="//##/"
+          isVisible={true}
+          customStyles=""
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/common/fields/button/CustomButton.jsx
+++ src/components/common/fields/button/CustomButton.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
-const CustomButton = () => {
+const CustomButton = ({
+  title,
+  isGradient,
+  varient,
+  url,
+  customStyles,
+  isVisible,
+}) => {
   return (
-    <div>CustomButton</div>
-  )
-}
+    <div className="inline-flex items-center">
+      {isVisible && (
+        <>
+          {varient === "button" ? (
+            <button
+              className={`font-inter font-semibold text-base w-full py-[10px] px-6 rounded overflow-hidden border border-solid border-light-yellow
+relative duration-300 ease-linear after:absolute after:w-full after:h-0 after:top-0 after:left-0 after:z-[-1] after:duration-300 after:ease-linear hover:after:h-full hover:after:bottom-0 leading-140 inline-block z-10
+        ${
+          isGradient
+            ? " bg-gradient text-white hover:border border-transparent after:bg-black   "
+            : " text-white opacity-90  bg-black hover:border-transparent after:bg-gradient"
+        } ${customStyles && customStyles}`}>
+              {title}
+            </button>
+          ) : (
+            <a
+              href={`${url}`}
+              aria-label=" botton for link or path"
+              className={`font-inter font-semibold text-base w-full py-[10px] px-6 rounded overflow-hidden border border-solid border-light-yellow
+relative duration-300 ease-linear after:absolute after:w-full after:h-0 after:top-0 after:left-0 after:z-[-1] after:duration-300 after:ease-linear hover:after:h-full hover:after:bottom-0 leading-140 inline-block z-10
+        ${
+          isGradient
+            ? " bg-gradient  text-white hover:border border-transparent after:bg-black   "
+            : " text-white opacity-90  bg-black hover:border-transparent after:bg-gradient"
+        } ${customStyles && customStyles}`}>
+              {title}
+            </a>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
