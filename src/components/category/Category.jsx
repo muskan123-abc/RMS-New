eb24baf505dbaf8e7a5d253c888ac78d61e519b2
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2024-02-14 15:27:07 +0530

    add all section except card

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -3,11 +3,17 @@ import Hero from "./Hero";
 import MostPopular from "./MostPopular";
 import Footer from "../common/Footer";
 import Faqs from "../common/Faqs";
+import IndianTvShows from "./IndianTvShows";
+import TvActionAdventure from "./TvActionAdventure";
+import RomanceTvShow from "./RomanceTvShow";
 const Category = () => {
   return (
     <>
       <Hero />
       <MostPopular />
+      <IndianTvShows />
+      <TvActionAdventure />
+      <RomanceTvShow/>
       <Faqs />
       <Footer />
     </>
