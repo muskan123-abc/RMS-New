e2a7ef7695207df9b409db5eac7ce6cfa92aba01
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-14 16:17:32 +0530

    side bar

--- src/components/common/sidebar/ProfileSidebar.jsx
+++ src/components/common/sidebar/ProfileSidebar.jsx
@@ -1,7 +1,40 @@
-import React from "react";
-
+import React from 'react'
+import ProfileImg from "../../../assets/images/sidebar/profile_img.png"
+import { SideList, SideTopList } from "../../../utils/Helper"
 const ProfileSidebar = () => {
-  return <div>ProfileSidebar</div>;
-};
+  return (
+    <>
+      <div className='w-[173px] h-screen bg-[#373022] pb-4 overflow-y-auto custom_Scroll_none'>
+        <div className='pt-5 pb-[10px] flex justify-center'>
+          <img className='w-14 h-[57px]' src={ProfileImg} alt="profile-img" />
+        </div>
+        <h2 className='ff_poppins text-lg leading-[156%] text-white text-center mb-5'>Floyd Miles</h2>
+        {SideTopList.map((obj, index) => {
+          return (
+            <div key={index}>
+              <div className='flex items-center justify-between mx-[10px] py-[10px] border-b border-[#59554C]'>
+                <h2 className='ff_poppins leading-[118.75%] text-sm text-white/80'>{obj.title}</h2>
+                <p className='ff_poppins leading-[118.75%] text-sm text-white/80'>{obj.rank}</p>
+              </div>
+            </div>
+          )
+        })}
+        <div className='mt-6'>
+          {SideList.map((obj, index) => {
+            return (
+              <div key={index} className='ps-4 mb-[10px] opacity-80 hover:opacity-100 hover:border-l-2 border-l-2 border-transparent hover:border-white/25 hover:bg-gradient-to-r from-white/10 to-white/10 cursor-pointer transition-all ease-linear duration-200'>
+                <div className='py-[10px] flex items-center gap-2 '>
+                  <span> {obj.image}</span>
+                  <h2 className='ff_poppins leading-[137%] text-white'>{obj.title}</h2>
+                </div>
+              </div>
+            )
+          }
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default ProfileSidebar;
+export default ProfileSidebar
--- src/utils/Helper.jsx
+++ src/utils/Helper.jsx
@@ -1,6 +1,12 @@
 //LANDING HERO LEFT SLIDER DATA
 import heroLeftImage1 from "../assets/images/webp/homepage_hero_left_slider_img1.webp";
-
+import { AccountIcon, HistoryIcon, HomeIcon, LibraryIcon, ReviewsIcon, SignOutIcon, SubscriptionIcon, TrendingIcon, WatchlistIcon } from "../components/common/icons/sidebar/SideBar"
+import JaneCooper from '../assets/images/sidebar/JaneCooper.png'
+import JeromeBell from '../assets/images/sidebar/JeromeBell.png'
+import GuyHawkins from '../assets/images/sidebar/GuyHawkins.png'
+import TheresaWebb from '../assets/images/sidebar/TheresaWebb.png'
+import SavannahNguyen from '../assets/images/sidebar/SavannahNguyen.png'
+import ArleneMcCoy from '../assets/images/sidebar/ArleneMcCoy.png'
 export const homeHeroLeftSlider = [
   {
     image: heroLeftImage1,
@@ -140,3 +146,133 @@ export const navDropdown = [
     url: "/category",
   },
 ];
+
+
+
+
+export const SideTopList = [
+  {
+    title: "Subscriptions",
+    rank: "100k",
+  },
+  {
+    title: "Uploads",
+    rank: "2.5k",
+  },
+  {
+    title: "Likes",
+    rank: "1.2M",
+  },
+]
+export const SideList = [
+  {
+    image: <HomeIcon />,
+    title: "Home"
+  },
+  {
+    image: <LibraryIcon />,
+    title: "Library"
+  },
+  {
+    image: <WatchlistIcon />,
+    title: "My Watchlist"
+  },
+  {
+    image: <ReviewsIcon />,
+    title: "My reviews"
+  },
+  {
+    image: <HistoryIcon />,
+    title: "History"
+  },
+  {
+    image: <SubscriptionIcon />,
+    title: "Subscription"
+  },
+  {
+    image: <TrendingIcon />,
+    title: "Trending"
+  },
+  {
+    image: <AccountIcon />,
+    title: "My account"
+  },
+  {
+    image: <SignOutIcon />,
+    title: "Sign out"
+  },
+]
+
+export const MessegeList = [
+  {
+    image: JaneCooper,
+    title: "Cooper",
+  },
+  {
+    image: JeromeBell,
+    title: "Jerome Bell",
+  },
+  {
+    image: GuyHawkins,
+    title: "Guy Hawkins",
+  },
+  {
+    image: TheresaWebb,
+    title: "Theresa Webb",
+  },
+  {
+    image: SavannahNguyen,
+    title: "Savannah Nguyen",
+  },
+  {
+    image: ArleneMcCoy,
+    title: "Arlene McCoy",
+  },
+  {
+    image: JaneCooper,
+    title: "Dianne Russell",
+  },
+  {
+    image: JeromeBell,
+    title: "Kathryn Murphy",
+  },
+  {
+    image: GuyHawkins,
+    title: "Jacob Jones",
+  },
+  {
+    image: TheresaWebb,
+    title: "Floyd Miles",
+  },
+  {
+    image: SavannahNguyen,
+    title: "Albert Flores",
+  },
+  {
+    image: ArleneMcCoy,
+    title: "Courtney Henry",
+  }, {
+    image: JaneCooper,
+    title: "Dianne Russell",
+  },
+  {
+    image: JeromeBell,
+    title: "Kathryn Murphy",
+  },
+  {
+    image: GuyHawkins,
+    title: "Jacob Jones",
+  },
+  {
+    image: TheresaWebb,
+    title: "Floyd Miles",
+  },
+  {
+    image: SavannahNguyen,
+    title: "Albert Flores",
+  },
+  {
+    image: ArleneMcCoy,
+    title: "Courtney Henry",
+  },
+]
