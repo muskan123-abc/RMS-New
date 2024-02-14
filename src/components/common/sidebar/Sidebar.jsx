a8b553458f1de45e249cfd04185d153f1da37b31
Rakesh Jaat <rakesh10448olla@gail.com>
2024-02-14 14:12:08 +0530

    code merge

--- src/components/common/sidebar/Sidebar.jsx
+++ src/components/common/sidebar/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
     <div
       className={`${
         showSidebar ? "  w-[414px] " : " w-0"
-      } flex overflow-hidden flex-col gap-10 items-center justify-center absolute min-[1350px]:sticky duration-300 min-[1350px]:left-0 top-0 z-40 h-screen bg-black`}>
+      } flex overflow-hidden flex-col gap-10 items-center justify-center  duration-300 min-[1350px]:left-0 top-0 z-40 h-screen bg-black`}>
       <button className="bg-red-800 p-4" onClick={() => setShowSidebar(false)}>
         sign out
       </button>
