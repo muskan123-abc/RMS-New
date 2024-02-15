c85d7f6d23a7106c10d8906ced26f305be872d0b
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-15 10:28:02 +0530

    side bar responsive

--- src/components/common/sidebar/ProfileSidebar.jsx
+++ src/components/common/sidebar/ProfileSidebar.jsx
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom'
 const ProfileSidebar = ({ setShowSidebar }) => {
   return (
     <>
-      <div className='w-full max-w-[173px] h-full bg-[#373022] pb-4 overflow-y-auto custom_Scroll_none'>
+      <div className='w-full max-[375px]:max-w-[150px] max-w-[173px] h-full bg-[#373022] pb-4 overflow-y-auto custom_Scroll_none'>
         <div className='pt-5 pb-[10px] flex justify-center'>
           <img className='w-14 h-[57px]' src={ProfileImg} alt="profile-img" />
         </div>
-        <h2 className='ff_poppins text-lg leading-[156%] text-white text-center mb-5'>Floyd Miles</h2>
+        <h2 className='ff_poppins max-[375px]:text-base text-lg leading-[156%] text-white  text-center mb-5'>Floyd Miles</h2>
         {sideTopList.map((obj, index) => {
           return (
             <div key={index}>
-              <div className='flex items-center justify-between mx-[10px] py-[10px] border-b border-[#59554C]'>
-                <h2 className='ff_poppins leading-[118.75%] text-sm text-white/80'>{obj.title}</h2>
-                <p className='ff_poppins leading-[118.75%] text-sm text-white/80'>{obj.rank}</p>
+              <div className='flex items-center justify-between max-[375px]:mx-2 mx-[10px] py-[10px] border-b border-[#59554C]'>
+                <h2 className='ff_poppins leading-[118.75%] max-[375px]:text-xs text-sm text-white/80'>{obj.title}</h2>
+                <p className='ff_poppins leading-[118.75%] max-[375px]:text-xs text-sm text-white/80'>{obj.rank}</p>
               </div>
             </div>
           )
@@ -23,12 +23,12 @@ const ProfileSidebar = ({ setShowSidebar }) => {
         <div className='mt-6'>
           {sideList.map((obj, index) => {
             return (
-              <div key={index} className='ps-4 mb-[10px] opacity-80 hover:opacity-100 hover:border-l-2 border-l-2 border-transparent hover:border-white/25 hover:bg-gradient-to-r from-white/10 to-white/10 cursor-pointer transition-all ease-linear duration-200 '>
+              <div key={index} className=' max-[375px]:ps-3 ps-4  mb-[10px] opacity-80 hover:opacity-100 hover:border-l-2 border-l-2 border-transparent hover:border-white/25 hover:bg-gradient-to-r from-white/10 to-white/10 cursor-pointer transition-all ease-linear duration-200 '>
                 <Link hrefLang='#' className='py-[10px] flex items-center gap-2 '
                   onClick={() => setShowSidebar(false)}
                 >
                   <span> {obj.image}</span>
-                  <h2 className='ff_poppins leading-[137%] text-white'>{obj.title}</h2>
+                  <h2 className='ff_poppins leading-[137%] max-[375px]:text-sm text-base text-white'>{obj.title}</h2>
                 </Link>
               </div>
             )
--- src/components/common/sidebar/Sidebar.jsx
+++ src/components/common/sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
   return (
     <div
       className={`${showSidebar ? "w-full" : "w-0"
-        } max-w-[414px] flex overflow-hidden items-center justify-center  duration-300 min-[1350px]:left-0 -bottom-[80px] z-40 h-[calc(100vh-80px)] bg-black fixed min-[1350px]:relative`}
+        } max-w-[414px] flex overflow-hidden items-center justify-center  duration-300 min-[1350px]:left-0 top-[80px] z-10 h-[calc(100vh-80px)] bg-black fixed min-[1350px]:relative`}
     >
       {/* <button className="bg-red-800 p-4" onClick={() => setShowSidebar(false)}>
         sign out
