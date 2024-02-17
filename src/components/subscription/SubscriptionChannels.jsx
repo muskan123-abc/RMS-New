20e70dec1dabf404c899d0d5684a9ca31ffb3380
mk0029 <mkhicher404@gmail.com>
2024-02-17 14:43:29 +0530

    /

--- src/components/subscription/SubscriptionChannels.jsx
+++ src/components/subscription/SubscriptionChannels.jsx
@@ -123,7 +123,7 @@ const SubscriptionChannels = () => {
                 929K Subscribers
               </h4>
               <CustomButton
-                isVisible={trugit e}
+                isVisible={true}
                 icon={<UnsubscribeBell />}
                 varient="button"
                 title="Unsubscribe"
