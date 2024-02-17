0fd52a12b543b73f709144e4628aff10192c197a
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2024-02-17 15:27:27 +0530

    some updates done

--- src/components/watchlist/WatchList.jsx
+++ src/components/watchlist/WatchList.jsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import WatchListCards from "./WatchListCards";
 import { WatchListBigIcon } from "../common/Icons";
 import CustomInput from "../../components/common/fields/CustomInput";
-import { watchListData } from "./Helper";
+import { watchListCardData } from "./Helper";
 const WatchList = () => {
   const [heartClicked, setHeartClicked] = useState([]);
 
   return (
     <>
-      <div className="bg-[url('/src/assets/images/webp/homepage_hero_bg_img.webp')] bg-no-repeat bg-center bg-cover relative pt-28 min-[1280px]:pt-[100px] z-10 pb-20">
-        <div className="mx-auto container md:max-w-full xl:max-w-[1140px] 2xl:max-w-[1320px] min-[2200px]:mx-auto max-[1439px]:mx-auto">
+      <div className="bg-[url('/src/assets/images/webp/homepage_hero_bg_img.webp')] bg-no-repeat bg-center bg-cover relative z-10 pt-28 md:pt-20 pb-12 xl:py-0">
+        <div className="mx-auto container md:max-w-full xl:max-w-[1140px] 2xl:max-w-[1320px] min-[2200px]:mx-auto max-[1439px]:mx-auto min-h-screen flex justify-center items-center flex-col">
           <div className="flex justify-between sm:items-center pb-8 w-full sm:flex-row flex-col gap-4">
             <h2 className="font-medium text-white text-xxl flex items-center gap-[11px]">
               <WatchListBigIcon />
@@ -23,7 +23,7 @@ const WatchList = () => {
             />
           </div>
           <div className="flex flex-wrap md:flex-nowrap w-100 gap-3 lg:gap-5 sm:mt-6">
-            {watchListData.map((item, index) => {
+            {watchListCardData.map((item, index) => {
               const isLiked = heartClicked.includes(index);
               return (
                 <>
