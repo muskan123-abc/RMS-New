e3a798f701131056c828b0975f06247f0489df58
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-17 12:21:35 +0530

    miner update

--- src/components/reviews/CommonCards.jsx
+++ src/components/reviews/CommonCards.jsx
@@ -11,7 +11,7 @@ const CommonCards = () => {
             <div
               key={index}
               className={`w-full  mb-5 sm:mb-0  lg:!w-[calc(33.3%_-_13px)] sm:!w-[calc(50%_-_13px)] !w-fill !px-0`}>
-              <div className=" border border-[#404040] rounded-xl py-5 sm:py-0 sm:pt-5 sm:pb-[42px] ps-4  pe-5 h-full hover:border-[#E1B862] duration-200">
+              <div className=" border border-shadow-gray-two rounded-xl py-5 sm:py-0 sm:pt-5 sm:pb-[42px] ps-4  pe-5 h-full hover:border-maximum-yellow-red duration-200">
                 <h5 className=" font-poppins font-semibold text-base text-white mb-0">
                   {data.title}
                 </h5>
